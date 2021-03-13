45a036d8bc3e48b070d7c772e95866d3496c2f57
HananCS <benhanan.subendran@gmail.com>
2021-03-13 11:43:18 +0530

    Hompage GridView components

--- lib/app/homepage.dart
+++ lib/app/homepage.dart
@@ -33,6 +33,95 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Demistyfying Batting Average',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[200],
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              height: 100,
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Unbouding Bowling Average',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              height: 100,
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'A Glance at D/L Law',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[200],
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              height: 100,
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Teams Ratings & Ranking',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              height: 100,
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Individual Ratings & Ranking',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[200],
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              height: 100,
+              width: 400,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
